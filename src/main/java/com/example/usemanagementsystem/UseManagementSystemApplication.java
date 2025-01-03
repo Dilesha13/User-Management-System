9495cb3ee898cc5c3183112de5ab60cce8d35b23
TharushiCypsoLab <tharushi_dilesha@cypsolabs.com>
2025-01-04 03:04:35 +0530

    Initialize user management system project.

---
+++ src/main/java/com/example/usemanagementsystem/UseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.usemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usemanagementsystem/UseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
