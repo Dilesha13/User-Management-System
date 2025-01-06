ad189f75c8e1d2a5691e7ea749775d9928b2ea2e
Dilesha13 <tharushidilesha1@gmail.com>
2025-01-06 16:58:07 +0530

    Declared ModelMapper as a Spring bean in the main application class

--- src/main/java/com/example/usemanagementsystem/UserManagementSystemApplication.java
+++ src/main/java/com/example/usemanagementsystem/UserManagementSystemApplication.java
@@ -1,13 +1,19 @@
 package com.example.usemanagementsystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class UseManagementSystemApplication {
+public class UserManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UseManagementSystemApplication.class, args);
+		SpringApplication.run(UserManagementSystemApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelmapper(){
+		return new ModelMapper();
 	}
 
 }
