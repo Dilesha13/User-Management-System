9899a152519cac05980f15572116305885978309
Dilesha13 <tharushidilesha1@gmail.com>
2025-01-06 17:27:52 +0530

    add UserRepository interface for database operations

--- src/main/java/com/example/usemanagementsystem/repo/UserRepository.java
+++ src/main/java/com/example/usemanagementsystem/repo/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.usemanagementsystem.repo;
 
-public interface UserRepository {
+import com.example.usemanagementsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
