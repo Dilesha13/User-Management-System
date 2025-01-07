d5fe246990c7d6d908f2274922250d30ccada07b
Dilesha13 <tharushidilesha1@gmail.com>
2025-01-07 23:38:54 +0530

    add get all user method to controller layer

--- src/main/java/com/example/usemanagementsystem/controller/UserController.java
+++ src/main/java/com/example/usemanagementsystem/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.example.usemanagementsystem.controller;
 
+import com.example.usemanagementsystem.dto.UserDTO;
+import com.example.usemanagementsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping(value="api/v1")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/getuser")
-    public String getUser() {
-        return "All users";
+    public List<UserDTO> getUser() {
+        return userService.getAllUsers();
     }
 }
